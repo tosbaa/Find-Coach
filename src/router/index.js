57bfa4a5eccbf6268429583463e4953dce187db6
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-25 21:45:07 +0300

    Set routers object

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,42 @@
 import { createRouter, createWebHistory } from "vue-router";
 const routes = [
-  
+  {
+    path: "/",
+    redirect: "/coaches"
+  },
+  {
+    path: "/coaches",
+    component: null,
+    children: [
+      {
+        path: ":id",
+        component: null,
+        children: [
+          {
+            path: "contact",
+            component: null
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: "/register",
+    component: null
+  },
+  {
+    path: "/requests",
+    component: null
+  },
+  {
+    path: "/coaches",
+    component: null
+  },
+
+  {
+    path: "/:notFound(.*)",
+    component: null
+  }
 ];
 
 const router = createRouter({
