84b9a6fd1048b4eb91638c3ac3448624a3f8b5f4
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-26 12:34:15 +0300

    Added request store

---
+++ src/store/modules/request/index.js
@@ -0,0 +1,13 @@
+import actions from "./actions.js";
+import getters from "./getters.js";
+import mutations from "./mutations.js";
+
+export default {
+  namespaced: true,
+  state() {
+    return {};
+  },
+  actions,
+  getters,
+  mutations
+};
