f2ab75990cf7b59085f906d4aefe94a4bd7574e2
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-26 11:58:17 +0300

    Made prototypes of pages and binded them together

--- src/views/coaches/CoachList.vue
+++ src/views/coaches/CoachList.vue
@@ -3,6 +3,10 @@
     FILTER
   </section>
   <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="register">Register As Coach</router-link>
+    </div>
     LIST OF COACHES
   </section>
 </template>
