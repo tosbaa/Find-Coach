1add894449ac4364e9258c73f6d14fb717902276
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-26 15:36:28 +0300

    Added base layout components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BaseCard from "./components/UI/BaseCard.vue";
+import BaseButton from "./components/UI/BaseButton.vue";
+import BaseBadge from "./components/UI/BaseBadge.vue";
 
 createApp(App)
   .use(store)
   .use(router)
+  .component("base-card", BaseCard)
+  .component("base-button", BaseButton)
+  .component("base-badge", BaseBadge)
   .mount("#app");
