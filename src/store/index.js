6377af9cec189f8e0f84c657a8789dbf6df2f582
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-26 12:34:32 +0300

    Bind modules to main store object

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from "vuex";
+import coach from "./modules/coach/index.js";
+import request from "./modules/request/index.js";
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: { coaches: coach, requests: request }
 });
