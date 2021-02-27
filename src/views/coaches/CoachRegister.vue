d598947676c66185e86eb38c5d7013028809571f
Yagiz Kocer <yagizkocer@gmail.com>
2021-02-27 14:27:53 +0300

    Binded form with coachregister component

---
+++ src/views/coaches/CoachRegister.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Register As Coach !</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from "../../components/coach/CoachForm.vue";
+export default {
+  components: {
+    CoachForm
+  }
+};
+</script>
