abad154fd7ece00f4ba2afb126bd9f230bf25316
Yagiz Kocer <yagizkocer@gmail.com>
2021-03-01 15:14:50 +0300

    Bind action methods with backend

--- src/store/modules/coach/actions.js
+++ src/store/modules/coach/actions.js
@@ -1,13 +1,30 @@
 export default {
-  saveCoach({ commit, rootGetters }, payload) {
+  async saveCoach({ commit, rootGetters }, payload) {
+    const userId = rootGetters.userId;
     const data = {
-      id: rootGetters.userId,
       firstName: payload.first,
       lastName: payload.last,
       description: payload.description,
       hourlyRate: payload.rate,
       areas: payload.areas
     };
-    commit("addCoach", data);
+
+    const response = await fetch(
+      `${process.env.VUE_APP_BACKEND_URL}/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(data)
+      }
+    );
+
+    //onst responseData = await response.json();
+    if (!response.ok) {
+      console.log("Error while saving coach");
+    }
+
+    commit("addCoach", {
+      id: userId,
+      ...data
+    });
   }
 };
