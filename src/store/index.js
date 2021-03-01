52e0d2257fcbb1b1de28c5bf9dc94b5fb22c28fa
Yagiz Kocer <yagizkocer@gmail.com>
2021-03-01 13:39:58 +0300

    Request component and logic added

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,11 @@ import coach from "./modules/coach/index.js";
 import request from "./modules/request/index.js";
 
 export default createStore({
-  state: {},
+  state() {
+    return {
+      userId: "c3"
+    };
+  },
   mutations: {},
   actions: {},
   modules: { coaches: coach, requests: request }
